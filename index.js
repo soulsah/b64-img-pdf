0e7f5d5f77ec72af8e5bf81f4a0206c45e1218a4
Matheus dos Santos Souza <ssouza.matheus@outlook.com>
2024-02-18 15:35:48 -0300

    quase terminando

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const { fromBase64 } = require('pdf2pic');
 const fs = require('fs');
 const imgToPDF = require('image-to-pdf');
+const PDFDocument = require('pdfkit');
+const pdftk = require('node-pdftk');
 
 const app = express();
 const port = 3000;
@@ -12,6 +14,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.post('/', async (req, res) => {
   try {
     const { pdf } = req.body;
+    const doc = new PDFDocument;
 
     const pdf2picOptions = {
       quality: 200,
@@ -38,10 +41,39 @@ app.post('/', async (req, res) => {
     imgToPDF(pages, imgToPDF.sizes.A4)
       .pipe(fs.createWriteStream(pdfConfig.output));
 
-    res.status(200).json({ success: true, msg: 'PDF convertido com sucesso.' });
+    // Exclui as imagens
+    pages.forEach(page => {
+      fs.unlinkSync(page);
+    })
+
+    //Cria um PDF para a nova página
+    doc.pipe(fs.createWriteStream('novaPagina.pdf'));
+    doc
+    .fontSize(27)
+    .text('Nova página do PDF', 100, 100);
+    doc.end();
+
+    var pdfBuffer1 = fs.readFileSync("./pdfOriginal.pdf");
+    var pdfBuffer2 = fs.readFileSync("./novaPagina.pdf");
+
+    pdftk
+        .input([pdfBuffer1, pdfBuffer2])
+        .output()
+        .then(buf => {
+            let path = 'pdfFinal.pdf';
+            fs.open(path, 'w', function (err, fd) {
+                fs.write(fd, buf, 0, buf.length, null, function (err) {
+                    fs.close(fd, function () {
+                        console.log('Hehehe');
+                    });
+                });
+            });
+        });
+
+    res.status(200).json({ success: true, msg: 'PDF gerado com sucesso.' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, error: 'Erro ao converter o PDF para imagem ou criar o PDF.' });
+    res.status(500).json({ success: false, error: 'Erro ao gerar PDF.' });
   }
 });
 
